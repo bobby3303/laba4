8bf5aa670ae7fc1e03e5673539a8760d07938457
Vladislav <kaigorodovvlad599@gmail.com>
2026-03-16 21:59:04 +0500

    lab4

---
+++ task1/Program.fs
@@ -0,0 +1,82 @@
+﻿open System
+let rnd = Random()
+
+type Tree = 
+    | Empty
+    | Node of int * Tree * Tree
+
+let rec dice d =
+    if d > 7 then 
+        Empty
+
+    else
+        let threshold = 
+            if d < 3 then 2    
+            elif d < 5 then 8  
+            else 15     
+            
+        if rnd.Next(1, 21) > threshold then
+            let v = rnd.Next(1,10001)
+            Node (v, dice(d + 1), dice(d + 1))
+        else
+            Empty
+
+let rec upDigit n =
+    if n < 10 then
+        if n = 9 then
+            9
+        else
+            n + 1
+    else 
+        let digit = n % 10
+        let remain = n / 10
+
+        let newDigit = 
+            if digit = 9 then 
+                9 
+            else 
+                digit + 1
+
+        (upDigit remain) * 10 + newDigit
+
+let rec mapTree tree =
+    match tree with
+    | Empty ->
+        Empty
+    | Node(v, l, r) ->
+        let newV = upDigit v
+        let newL = mapTree l
+        let newR = mapTree r
+
+        Node (newV, newL, newR)
+
+let rec print d tree =
+    match tree with
+    | Empty -> ()
+    | Node(v, l, r) ->
+        let indent = String.replicate d "  "
+        printfn "%s|-- %d" indent v
+        
+        print (d + 1) l
+        print (d + 1) r
+
+
+
+[<EntryPoint>]
+let main args =
+    let source = dice 1
+    
+    if source = Empty then 
+        printfn "Дерево пустое, попробуйте запустить еще раз"
+    else
+        printfn "ИСХОДНОЕ ДЕРЕВО:"
+        print 0 source
+
+        // Применяем мап
+        let result = mapTree source
+        printfn "\nОБРАБОТАННОЕ ДЕРЕВО (разряды +1, кроме 9):"
+        print 0 result
+
+    0
+
+
---
+++ task2/Program.fs
@@ -0,0 +1,85 @@
+﻿open System
+let rnd = Random()
+
+type STree = 
+    | Empty
+    | Node of string * STree * STree
+
+let rec getSymb () =
+    printf "Введите символ для поиска: "
+    let input = Console.ReadLine()
+    if String.IsNullOrEmpty(input) then
+        printfn "Ошибка: вы ничего \
+         не ввели. Попробуйте снова."
+        getSymb()
+    else
+        input.[0] 
+
+let gen () =
+    let chars = "abcdefgABCDEFG0123456789!@#$%^&*"
+    let len = rnd.Next(0, 8)
+    if len = 0 then 
+        "" 
+    else 
+        [| for _ in 1..len -> chars.[rnd.Next(chars.Length)] |]
+        |> String
+
+let rec dice d =
+    if d > 7 then 
+        Empty
+
+    else
+        let threshold = 
+            if d < 3 then 2    
+            elif d < 5 then 8  
+            else 15    
+            
+        if rnd.Next(1, 21) > threshold then
+            let v = gen ()
+            Node (v, dice(d + 1), dice(d + 1))
+        else
+            Empty
+
+let rec count (c: char) tree =
+    match tree with
+    | Empty -> 0
+    | Node(s, l, r) ->
+        let cur = 
+            if s.Contains(c) then 
+                0 
+            else 
+                1
+        cur + (count c l) + (count c r)
+
+let rec print d tree =
+    match tree with
+    | Empty -> ()
+    | Node(s, l, r) ->
+        let indent = String.replicate d "  "
+        let outStr = 
+            if s = "" then 
+                "\"\"" 
+            else 
+                s
+        printfn "%s|-- %s" indent outStr
+        print(d + 1) l
+        print (d + 1) r
+
+
+[<EntryPoint>]
+let main argv =
+    let source = dice 1
+    
+    if source = Empty then
+        printfn "Дерево пустое, попробуйте запустить еще раз"
+    else
+        printfn "Сгенерированное дерево строк:"
+        print 0 source
+        
+        printfn "\n--------------------------------------"
+        let symb = getSymb()
+        let result = count symb source
+
+        printfn "\nКоличество узлов, в которых \
+         НЕТ символа '%c': %d" symb result
+    0
