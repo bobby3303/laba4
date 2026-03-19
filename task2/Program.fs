a233ab0465749c5c75d62049920a4ee769b7fbec
bobby3303 <kaigorodovvlad599@gmail.com>
2026-03-19 20:05:12 +0500

    lab4

--- task2/Program.fs
+++ task2/Program.fs
@@ -1,19 +1,17 @@
-﻿open System
+open System
 let rnd = Random()
 
 type STree = 
     | Empty
     | Node of string * STree * STree
 
-let rec getSymb () =
-    printf "Введите символ для поиска: "
-    let input = Console.ReadLine()
-    if String.IsNullOrEmpty(input) then
-        printfn "Ошибка: вы ничего \
-         не ввели. Попробуйте снова."
-        getSymb()
-    else
-        input.[0] 
+let rec insert tree x =
+    match tree with
+    | Empty -> Node(x, Empty, Empty)
+    | Node(v, l, r) ->
+        if x < v then Node(v, insert l x, r)
+        elif x > v then Node(v, l, insert r x)
+        else Node(v, l, r) 
 
 let gen () =
     let chars = "abcdefgABCDEFG0123456789!@#$%^&*"
@@ -24,32 +22,24 @@ let gen () =
         [| for _ in 1..len -> chars.[rnd.Next(chars.Length)] |]
         |> String
 
-let rec dice d =
-    if d > 7 then 
-        Empty
-
+let rec genTree n tree =
+    if n = 0 then 
+        tree
     else
-        let threshold = 
-            if d < 3 then 2    
-            elif d < 5 then 8  
-            else 15    
-            
-        if rnd.Next(1, 21) > threshold then
-            let v = gen ()
-            Node (v, dice(d + 1), dice(d + 1))
-        else
-            Empty
+        let value = gen() 
+        genTree (n - 1) (insert tree value)
 
-let rec count (c: char) tree =
+let rec fold f acc tree =
     match tree with
-    | Empty -> 0
-    | Node(s, l, r) ->
-        let cur = 
-            if s.Contains(c) then 
-                0 
-            else 
-                1
-        cur + (count c l) + (count c r)
+    | Empty -> acc
+    | Node(v, l, r) ->
+        let accLeft = fold f acc l
+        let accCur = f accLeft v
+        fold f accCur r
+
+
+let count (c: char) n (s: string) =
+    if s.Contains(c) then n else n + 1
 
 let rec print d tree =
     match tree with
@@ -65,10 +55,20 @@ let rec print d tree =
         print(d + 1) l
         print (d + 1) r
 
+let rec getSymb () =
+    printf "Введите символ для поиска: "
+    let input = Console.ReadLine()
+    if String.IsNullOrEmpty(input) then
+        printfn "Ошибка: вы ничего \
+         не ввели. Попробуйте снова."
+        getSymb()
+    else
+        input.[0] 
+
 
 [<EntryPoint>]
 let main argv =
-    let source = dice 1
+    let source = genTree (rnd.Next(0, 24)) Empty
     
     if source = Empty then
         printfn "Дерево пустое, попробуйте запустить еще раз"
@@ -78,7 +78,7 @@ let main argv =
         
         printfn "\n--------------------------------------"
         let symb = getSymb()
-        let result = count symb source
+        let result = fold (count symb) 0 source
 
         printfn "\nКоличество узлов, в которых \
          НЕТ символа '%c': %d" symb result
